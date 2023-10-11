0f1004cda95d777c653b26d47caee8190366022b
PankajBhatta <96721563+PankajBhatta@users.noreply.github.com>
2023-10-11 11:24:21 +0545

    callback

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const { MongoClient } = require("mongodb");
 const url = 'mongodb://localhost:27017';
-const database  = 'employees'
+const database = 'employees'
 const client = new MongoClient(url);
 
 async function dbconnect()
 {
-    let result = await client.connect();
-    let db=result.db(database)
-    let collection = db.collection('managers');
-   // let response = await collection.find({}).toArray();
-   // console.log(Data);
-//    ..
+  let result = await client.connect();
+  let db=result.db(database)
+  let collection = db.collection('managers');
+  return collection;
 }
-dbconnect().then((resp) => { 
-    console.warn(resp.find().toArray())
+
+dbconnect().then((collection) => {
+  console.warn(collection.find().toArray());
 });
+
+console.log("Hello World!!");
